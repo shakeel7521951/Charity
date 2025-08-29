5cb21a3ed50c5279b044157db9c28ba36ee191e5
fakharabbasbhatti <fakharabbasbhatti333@gmail.com>
2025-08-29 04:44:45 -0700

    charity projects pages complete login,signup & contact

---
+++ app/contact/page.jsx
@@ -0,0 +1,13 @@
+import Hero from '../_components/contact/Hero';
+import Contact_Form from '../_components/contact/Contact_Form';
+import Map from '../_components/contact/Map';
+
+export default function Page() {
+  return (
+    <div>
+      <Hero />
+      <Contact_Form />
+      <Map />
+    </div>
+  );
+}
