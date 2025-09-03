16dbdc1a3487a66c036407cbfbeec713c90b3b97
abdul0325 <habdul2266@gmail.com>
2025-09-03 11:35:31 +0500

    dashboard sidebar

---
+++ app/dashboard/donors/page.jsx
@@ -0,0 +1,12 @@
+import DonorsPage from '@/app/_components/dashboard/donors/page'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            <DonorsPage />
+        </div>
+    )
+}
+
+export default page
